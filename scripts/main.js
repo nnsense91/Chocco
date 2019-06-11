7208b0fc44b14ab353024cdc823c22beea705e75
Ivan Egorov <HD-74@yandex.ru>
2019-06-11 13:22:50 +0500

    Map yandex added

--- scripts/main.js
+++ scripts/main.js
@@ -422,3 +422,52 @@ setTimeout(function() {
     }
   });
 }, 2000);
+
+//map
+
+ymaps.ready(init);
+let myPlacemark;
+function init() {
+  // Создание карты.
+  let myMap = new ymaps.Map("map", {
+    center: [55.16082, 61.401933],
+    zoom: 11
+  });
+  myMap.behaviors.disable("scrollZoom");
+  myPlacemark1 = new ymaps.Placemark(
+    [55.155455, 61.413132],
+    {
+      hintContent: "Точка продажи №1"
+    },
+    {
+      iconLayout: "default#image",
+      iconImageHref: "./img/contacts/map-mark.png",
+      iconImageSize: [46, 57]
+    }
+  );
+  myPlacemark2 = new ymaps.Placemark(
+    [55.160746, 61.448295],
+    {
+      hintContent: "Точка продажи №2"
+    },
+    {
+      iconLayout: "default#image",
+      iconImageHref: "./img/contacts/map-mark.png",
+      iconImageSize: [46, 57]
+    }
+  );
+  myPlacemark3 = new ymaps.Placemark(
+    [55.24896, 61.393678],
+    {
+      hintContent: "Точка продажи №3"
+    },
+    {
+      iconLayout: "default#image",
+      iconImageHref: "./img/contacts/map-mark.png",
+      iconImageSize: [46, 57]
+    }
+  );
+  myMap.geoObjects.add(myPlacemark1);
+  myMap.geoObjects.add(myPlacemark2);
+  myMap.geoObjects.add(myPlacemark3);
+}
